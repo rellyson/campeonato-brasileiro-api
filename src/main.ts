75db879387893a4688a2ef4c8eac2fbe18a345d6
Bruno Leone A P Corrêa <gerencia@acetime.com.br>
2022-03-12 19:00:57 -0300

    Adicionando informações de tempo

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Hello World!');
+    res.json({ code: 200, status: 'OK', dateTime: new Date().toISOString() });
 });
 
 app.listen(port, () => {
